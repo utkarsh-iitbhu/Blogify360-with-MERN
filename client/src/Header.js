4b93ae5a08c289971318f112046ce150f655b249
Utkarsh <utkarsh.bhuiit@gmail.com>
2023-07-25 01:52:22 +0530

    Added files

---
+++ client/src/Header.js
@@ -0,0 +1,47 @@
+import { useEffect, useState,useContext } from "react";
+import { Link } from "react-router-dom";
+import {UserContext} from "./UserContext";
+
+export default function Header() {
+  // const [username, setUserName] = useState(null);
+  const {setUserInfo,userInfo} = useContext(UserContext);
+  useEffect(() => {
+    fetch("http://localhost:4000/profile",{
+      credentials: "include"
+    }).then(response =>{
+      response.json().then(userInfo =>{
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
+
+  function logout(){
+    fetch('http://localhost:4000/logout',{
+      credentials : 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;   
+
+  return (
+    <header>
+      <Link to="/" className="logo">MyBlog</Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+export default function RegisterPage() {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  async function register(ev) {
+    ev.preventDefault();
+    const response = await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.status === 200) {
+      alert("Registration successful");
+    } else {
+      alert("Registration failed");
+    }
+  }
+  return (
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(ev) => setUserName(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <button>Register</button>
+    </form>
+  );
+}
